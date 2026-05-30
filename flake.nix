fed4194621679d58b0b2f8b11f0f9c8717fcb011
gabbelitoV2 <itzgabbe@gmail.com>
2026-05-30 11:26:23 +0000

    chore: prepare release 0.45.4 - update version in package files and scripts

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let  
         pkgs = import nixpkgs { system = system; config.allowUnfree = true; };
-        cloudypadVersion = "0.45.3";
+        cloudypadVersion = "0.45.4";
       in {
         packages = rec {
           default = cloudypad;
@@ -19,7 +19,7 @@
 
             src = pkgs.fetchurl {
               url = "https://raw.githubusercontent.com/gabbelitoV2/cloudypad/v${cloudypadVersion}/cloudypad.sh";
-              hash = "sha256:1rs6jvx8k4l5jshniw672nf9c0475wjc17xfwasv49d55yl6922f";
+              hash = "sha256:05mrxv80cbcdqa7ly9xhj7kmbfrswi8f7dapfx2z9ybw4csjqhrh";
             };
 
             phases = [ "installPhase" ];
